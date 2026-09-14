98e258452fa1211bd2b8412ef90c809ddac013fb
zaida-hutchings <zaida.hutchings@gmail.com>
2026-09-14 15:00:01 -0400

    Update script.js revert test

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-/(function ($) {
-
-  function showRegion() {
-    // Hide every section
-    $('.content-region')
-      .removeClass('show')
-      .addClass('hide')
-      .hide();
-
-    // Remove active state from menu
+/ Wait for the document to load before running the script 
+(function ($) {
+  
+  // We use some Javascript and the URL #fragment to hide/show different parts of the page
+  // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Linking_to_an_element_on_the_same_page
+  $(window).on('load hashchange', function(){
+    
+    // First hide all content regions, then show the content-region specified in the URL hash 
+    // (or if no hash URL is found, default to first menu item)
+    $('.content-region').hide();
+    
+    // Remove any active classes on the main-menu
     $('.main-menu a').removeClass('active');
+    var region = location.hash.toString() || $('.main-menu a:first').attr('href');
+    
+    // Now show the region specified in the URL hash
+    $(region).show();
+    
+    // Highlight the menu link associated with this region by adding the .active CSS class
+    $('.main-menu a[href="'+ region +'"]').addClass('active'); 
 
-    // Get the section from the URL
-    var region = location.hash || $('.main-menu a:first').attr('href');
-
-    // Make sure the requested section actually exists
-    if ($(region).length) {
-      $(region)
-        .removeClass('hide')
-        .addClass('show')
-        .show();
-
-      // Highlight the matching main navigation link
-      $('.main-menu a[href="' + region + '"]').addClass('active');
-    }
-  }
-
-  $(window).on('load hashchange', showRegion);
-
+    // Alternate method: Use AJAX to load the contents of an external file into a div based on URL fragment
+    // This will extract the region name from URL hash, and then load [region].html into the main #content div
+    // var region = location.hash.toString() || '#first';
+    // $('#content').load(region.slice(1) + '.html')
+    
+  });
+  
 })(jQuery);
